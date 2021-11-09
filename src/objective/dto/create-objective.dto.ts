134ef5d4302485d79cc64f5f10321d207f1eb52e
Antonio <antoniocar16@hotmail.com>
2021-11-08 21:31:22 -0300

    feat(create-dto): create Objective create-dto

---
+++ src/objective/dto/create-objective.dto.ts
@@ -0,0 +1,29 @@
+import { IsDateString, IsInt, IsString, Length } from 'class-validator';
+
+export class CreateObjectiveDto {
+  @IsString()
+  @Length(2, 200)
+  name: string;
+
+  @IsString()
+  @Length(2, 1000)
+  description: string;
+
+  @IsDateString()
+  startDate: string;
+
+  @IsDateString()
+  endDate: string;
+
+  @IsInt()
+  teamID: number;
+
+  @IsInt({ each: true })
+  relationalObjectives?: number[];
+
+  @IsInt({ each: true })
+  quarters: number[];
+
+  @IsInt()
+  managerId: number;
+}
