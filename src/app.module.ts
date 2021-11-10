08f58003ac1d3f6d05f3d7e9d5953e37ef9aad35
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2021-11-09 21:54:54 -0300

    refactor[app.module]: delete tasks, quarter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { TasksModule } from './tasks/tasks.module';
 import { KeyresultsModule } from './keyresults/keyresults.module';
 import { CheckinModule } from './checkin/checkin.module';
 import { TeamPartnerModule } from './team-partners/team-partners.module';
 import { TeamModule } from './team/team.module';
-import { QuarterModule } from './quarter/quarter.module';
 
 @Module({
   imports: [
     PrismaModule,
-    TasksModule,
     KeyresultsModule,
     CheckinModule,
     TeamPartnerModule,
     TeamModule,
-    QuarterModule,
   ],
   controllers: [AppController],
   providers: [AppService],
