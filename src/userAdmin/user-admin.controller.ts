2f0e0ecb4c07cf1c27ff2cf733bec097e068125c
Bianca Monteiro Corrêa <biancamonteiro22.06@gmail.com>
2021-11-10 21:48:34 -0300

    refactor[controller]: add login, modify Patch

--- src/userAdmin/user-admin.controller.ts
+++ src/userAdmin/user-admin.controller.ts
@@ -5,12 +5,15 @@ import {
   Delete,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { UserAdminDto } from './dto/create-userAdmin.dto';
+import { CredentialsDto, credentialsResponse } from './dto/credentials.dto';
+import { UpdateUserAdminDto } from './dto/update-user-admin.dto';
 import { UserAdminService } from './user-admin.service';
 
 @Controller('user-admin')
@@ -23,16 +26,21 @@ export class UserAdminController {
     return this.userAdminService.createUserAdmin(createUserAdmin);
   }
 
+  @Post('/login')
+  login(@Body() data: CredentialsDto): Promise<credentialsResponse> {
+    return this.userAdminService.login(data);
+  }
+
   @Delete('/delete/:id')
   @UsePipes(ValidationPipe)
   async delete(@Param('id') id: string) {
     return this.userAdminService.deleteOneUserAdmin({ id: Number(id) });
   }
 
-  @Put('/update/:id')
+  @Patch('/update/:id')
   @UsePipes(ValidationPipe)
   async update(
-    @Body() updateUserAdmin: UserAdminDto,
+    @Body() updateUserAdmin: UpdateUserAdminDto,
     @Param('id', ParseIntPipe) id: number,
   ): Promise<userAdmin> {
     return this.userAdminService.updateOneUserAdmin(id, updateUserAdmin);
