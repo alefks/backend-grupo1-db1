27a535449b082228aecd8e72bb05290fc315fabe
BritosMarco <marco.britocn@gmail.com>
2021-11-17 21:33:26 -0300

    refactor  [cors]: remove / from back end link

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:3000', 'https://db1-okr.herokuapp.com/'],
+      origin: ['http://localhost:3000', 'https://db1-okr.herokuapp.com'],
     },
   });
 
