31e3c33aab27713868d02df38be3dd864db72be1
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2021-11-26 16:56:32 -0300

    feat[update-objective.dto]: add disconnectObjective

--- src/objective/dto/update-objective.dto.ts
+++ src/objective/dto/update-objective.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateObjectiveDto } from './create-objective.dto';
+import { IsOptional, IsNumber } from 'class-validator';
 
-export class UpdateObjectiveDto extends PartialType(CreateObjectiveDto) {}
+export class UpdateObjectiveDto extends PartialType(CreateObjectiveDto) {
+  @IsOptional()
+  @IsNumber()
+  disconnectObjective?: number;
+}
